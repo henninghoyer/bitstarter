06414ddccd027dcad408886bec0b38f125d0bb6b
Henning Hoyer <henninghoyer@gmail.com>
2013-07-27 22:59:55 +0200

    Modified to include static files

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
+process.env.PWD = process.cwd();
 var fs = require('fs');
 var iFile = "index.html";
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use('/js',express.static(process.env.PWD+'/js'));
+app.use('/styles',express.static(process.env.PWD+'/styles'));
+app.use('/images',express.static(process.env.PWD+'/images'));
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync(iFile).toString());
